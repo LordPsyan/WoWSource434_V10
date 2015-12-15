6e457db84ccc4fec73ca42dfae3d8e1f2e2dbd1c
LordPsyan <realmsofwarcraft@gmail.com>
2015-12-15 09:15:39 -0600

    Fixed compile in Linux. Replaced with newer jemalloc and changed *restricted to *__restricted__ so this stupid thing would compile. Not sure why this errored, because *restrcited is used in TC. Maybe it is an ACE thing?

--- dep/jemalloc/src/zone.c
+++ dep/jemalloc/src/zone.c
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2013-2015 DeathCore <http://www.noffearrdeathproject.net/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "jemalloc/internal/jemalloc_internal.h"
 #ifndef JEMALLOC_ZONE
 #  error "This source file is for zones on Darwin (OS X)."
